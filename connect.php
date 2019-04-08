e38327a07faea8f41d5f2936734e5bfd90f9b3e5
Jack Vincent Hall <jackvhall@outlook.com>
2019-04-07 20:36:08 -0700

    tinkering

--- connect.php
+++ connect.php
@@ -1,15 +1,15 @@
 <?php
 session_start();
 
-DEFINE('DB_user', 'phpappuser@jackvhallmysql');
-DEFINE('DB_PWD', 'MySQLAzure2019');
+DEFINE('DB_user', 'jackh@jackvhallmysql');
+DEFINE('DB_PWD', 'Rivers360');
 DEFINE('DB_host', 'jackvhallmysql.mysql.database.azure.com');
 DEFINE('DB_name', 'blog_project');
 
 $conn = new mysqli();
 $conn->init();
-$conn->ssl_set(NULL,NULL, "/var/www/html/BaltimoreCyberTrustRoot.crt.pem", NULL, NULL);
-// $conn->ssl_set(NULL,NULL, "C:/ssl/BaltimoreCyberTrustRoot.crt.pem", NULL, NULL) ;
+// $conn->ssl_set(NULL,NULL, "/var/www/html/BaltimoreCyberTrustRoot.crt.pem", NULL, NULL);
+$conn->ssl_set(NULL,NULL, "BaltimoreCyberTrustRoot.crt.pem", NULL, NULL) ;
 $conn->real_connect($DB_host, $DB_user, $DB_PWD, $DB_name, 3306, MYSQLI_CLIENT_SSL);
 
 if ($conn->connect_error) {
