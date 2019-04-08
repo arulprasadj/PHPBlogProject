6049d9d60b175c8391ebcaf5977852a40d56d727
Jack Vincent Hall <jackvhall@outlook.com>
2019-04-07 20:18:36 -0700

    msft recommended config for connect file

--- connect.php
+++ connect.php
@@ -6,10 +6,10 @@ DEFINE('DB_PWD', 'MySQLAzure2019');
 DEFINE('DB_host', 'jackvhallmysql.mysql.database.azure.com');
 DEFINE('DB_name', 'blog_project');
 
-$conn = new mysqli(DB_host, DB_user, DB_PWD, DB_name);
-
-$conn = mysqli_init();
-$conn->ssl_set(NULL,NULL, "/var/www/html/BaltimoreCyberTrustRoot.crt.pem", NULL, NULL) ;
+$conn = new mysqli();
+$conn->init();
+$conn->ssl_set(NULL,NULL, "/var/www/html/BaltimoreCyberTrustRoot.crt.pem", NULL, NULL);
+// $conn->ssl_set(NULL,NULL, "C:/ssl/BaltimoreCyberTrustRoot.crt.pem", NULL, NULL) ;
 $conn->real_connect($DB_host, $DB_user, $DB_PWD, $DB_name, 3306, MYSQLI_CLIENT_SSL);
 
 if ($conn->connect_error) {
