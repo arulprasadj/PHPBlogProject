124e3ab4aa8512ff97af54e2daf28e7fa0f26401
Jack Vincent Hall <jackvhall@outlook.com>
2019-04-07 20:42:44 -0700

    specified SSL options and performed init() on mysqli

--- connect.php
+++ connect.php
@@ -8,9 +8,8 @@ DEFINE('DB_name', 'blog_project');
 
 $conn = new mysqli();
 $conn->init();
-// $conn->ssl_set(NULL,NULL, "/var/www/html/BaltimoreCyberTrustRoot.crt.pem", NULL, NULL);
-$conn->ssl_set(NULL,NULL, "BaltimoreCyberTrustRoot.crt.pem", NULL, NULL) ;
-$conn->real_connect($DB_host, $DB_user, $DB_PWD, $DB_name, 3306, MYSQLI_CLIENT_SSL);
+$conn->ssl_set(NULL,NULL, "/var/www/html/BaltimoreCyberTrustRoot.crt.pem", NULL, NULL);
+$conn->real_connect(DB_host, DB_user, DB_PWD, DB_name, 3306, MYSQLI_CLIENT_SSL);
 
 if ($conn->connect_error) {
     die ("Could not connect. ". $conn->connect_error);
